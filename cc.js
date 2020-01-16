be0df8b9a8514727fe629c356386b12d60b78567
chaitanya4419 <54065162+chaitanya4419@users.noreply.github.com>
2020-01-16 15:32:13 +0530

    Update cc.js

--- cc.js
+++ cc.js
@@ -13,13 +13,9 @@ pipeline {
 		    steps {
 			       sh '''
 				    which ibmcloud
-                    #ibmcloud login -a "https://cloud.ibm.com" --apikey eo4d1315DZHzteNKoFhRsQ-ZL0FSfgfZLkSSuyUcp0Cq -r eu-gb
 					ibmcloud login -a "https://cloud.ibm.com" --apikey tNRRYqdjFs4WdD516EUeWQ1o1ox2vKjeFhJTWwT7PxPA -r eu-gb 
-                    #ibmcloud plugin install cloud-functions
-                    #ibmcloud target -o vidhprak@in.ibm.com -s dev
 					ibmcloud target-o chaicho1@in.ibm.com -s dev -g Default-cc
 					ibmcloud fn list
-					#cd cloud-fn
 					pwd
 					#ibmcloud fn action create hello1 hello.js --kind nodejs:10
                  '''
